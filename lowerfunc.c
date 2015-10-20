388a8ea9d4a8373e43139a3fbfa3201a1a562914
Josh Rupprecht <ruppjo01@luther.edu>
2015-10-20 16:27:54 -0500

    Working on the program
    Still does't work. returns nothing for capital letters

--- lowerfunc.c
+++ lowerfunc.c
@@ -1,6 +1,10 @@
 #include "lowerfunc.h"
 
 int lowerfunc(char c) {
+ 
+  if (c >= 'A' && c <= 'Z')
+      c = (c + 32);
+      return c;
 
-
+  return 1;
 }
--- lowermain.c
+++ lowermain.c
@@ -5,4 +5,15 @@ int main(int argc, char* argv[]) {
   
   char c;
 
+  c = getchar();
+
+  while (c != EOF) {
+
+      if (lowerfunc(c) == c)
+        printf("%c",c);
+
+      c = getchar();
+   }
+   
+   return 0;
 }
